bfcfb395b7edfd00beeda21fb715a23e6afc58f9
arseni.korobejnikov@yandex.ru <arseni.korobejnikov@yandex.ru>
2025-10-13 14:21:03 +0000

    hot fix bug with not message update

--- Python-bot/bot/__main__.py
+++ Python-bot/bot/__main__.py
@@ -18,16 +18,24 @@ def main() -> None:
             updates = bot.telegram_client.getUpdates(updates_next_offset)
             bot.database_client.persist_updates(updates)
             updates_next_offset = get_next_offset(updates)
+            
             for update in updates:
+                if "message" not in update:
+                    print("x", end="", flush=True)
+                    continue                
+                chat_id = update["message"]["chat"]["id"]
+                
                 if "text" in update["message"]:
                     message_text = update["message"]["text"]
                 else:
                     message_text = "Это фото"
+
                 bot.telegram_client.sendMessage(
-                    chat_id=update["message"]["chat"]["id"],
+                    chat_id=chat_id,
                     text=message_text,
                 )
                 print(".", end="", flush=True)
+            
             time.sleep(1)
     except KeyboardInterrupt:
         print("\nBye!")
