a8efb5b47c0bc16df9c9828cee43dcf2b7c4c240
arseni.korobejnikov@yandex.ru <arseni.korobejnikov@yandex.ru>
2025-11-17 12:24:22 +0000

    add dispatcher and abstract handler

--- Python-bot/bot/handler.py
+++ Python-bot/bot/handler.py
@@ -1,9 +1,13 @@
-from abc import ABC]
+from abc import ABC, abstractmethod
+
+from bot.handler_result import HandlerStatus
+
 
 class Handler(ABC):
     @abstractmethod
-    def can_handle(self, update:dict) -> bool: ...
+    def can_handle(self, update: dict) -> bool:
+        pass
 
     @abstractmethod
-    def handle(self, update: dict) -> bool:
+    def handle(self, update: dict) -> HandlerStatus:
         pass
