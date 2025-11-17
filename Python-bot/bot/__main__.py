b464aec86489d1f568c767c359e75cd6fb06ad05
arseni.korobejnikov@yandex.ru <arseni.korobejnikov@yandex.ru>
2025-11-17 12:31:00 +0000

    new main with start long polling with dispatcher

--- Python-bot/bot/__main__.py
+++ Python-bot/bot/__main__.py
@@ -1,45 +1,14 @@
-import time
-
-import bot.database_client
-import bot.telegram_client
-
-
-def get_next_offset(updates: dict) -> int:
-    next_offset = 0
-    for update in updates:
-        next_offset = max(next_offset, update["update_id"] + 1)
-    return next_offset
-
+import bot.long_polling
+from bot.dispatcher import Dispatcher
+from bot.handlers import get_handlers
 
 def main() -> None:
     try:
-        updates_next_offset = 0
-        while True:
-            updates = bot.telegram_client.getUpdates(offset=updates_next_offset)
-            bot.database_client.persist_updates(updates)
-            updates_next_offset = get_next_offset(updates)
-            
-            for update in updates:
-                if "message" not in update:
-                    print("x", end="", flush=True)
-                    continue                
-                chat_id = update["message"]["chat"]["id"]
-                
-                if "text" in update["message"]:
-                    message_text = update["message"]["text"]
-                else:
-                    message_text = "Это фото"
-
-                bot.telegram_client.sendMessage(
-                    chat_id=chat_id,
-                    text=message_text,
-                )
-                print(".", end="", flush=True)
-            
-            time.sleep(1)
+        dispatcher = Dispatcher()
+        dispatcher.add_handlers(*get_handlers())
+        bot.long_polling.start_long_polling(dispatcher)
     except KeyboardInterrupt:
         print("\nBye!")
 
-
 if __name__ == "__main__":
-    main()
+    main() 
