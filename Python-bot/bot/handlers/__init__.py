dd1654daf6e347220a87e819d086b80e76c61d27
arseni.korobejnikov@yandex.ru <arseni.korobejnikov@yandex.ru>
2025-11-21 08:01:36 +0000

    add new handler Order_approval

--- Python-bot/bot/handlers/__init__.py
+++ Python-bot/bot/handlers/__init__.py
@@ -5,6 +5,8 @@ from bot.handlers.message_start import MessageStart
 from bot.handlers.pizza_selection import PizzaSelectionHandler
 from bot.handlers.pizza_size import PizzaSizeHandler
 from bot.handlers.pizza_drinks import PizzaDrinksHandler
+from bot.handlers.order_approval import OrderApprovalHandler
+
 
 def get_handlers() -> list[Handler]:
     return [
@@ -14,4 +16,5 @@ def get_handlers() -> list[Handler]:
         PizzaSelectionHandler(),
         PizzaSizeHandler(),
         PizzaDrinksHandler(),
+        OrderApprovalHandler(),
     ]
